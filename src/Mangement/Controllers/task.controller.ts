2874ca8af049629fb4568cc43bf7eebe3b1e5aff
satheeswaran <jpsathiswaran@gmail.com>
2023-12-22 18:13:50 +0530

    feat(Engine):completed

--- src/Mangement/Controllers/task.controller.ts
+++ src/Mangement/Controllers/task.controller.ts
@@ -1,45 +1,47 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Post,
-  Put,
-} from '@nestjs/common';
-import { Task } from '../Entities/task.entity';
-import { TaskService } from '../Services/task.service';
+import { Body, Controller, Delete, Get, Param, Post, Put, Query } from "@nestjs/common";
+import { Task } from "../Entities/task.entity";
+import { TaskService } from "../Services/task.service";
 
-@Controller('management/tasks')
+@Controller("management/tasks")
 export class TaskController {
-  constructor(private readonly taskService: TaskService) {}
+	constructor(private readonly taskService: TaskService) {}
 
-  @Get()
-  async findAll(): Promise<Task[]> {
-    console.log('test');
-    return this.taskService.findAll();
-  }
+	@Get(":limit/:page")
+	async findAll(
+		@Param("limit") limit: number,
+		@Param("page") page: number,
+		@Query("total") total: string
+	): Promise<{ data: Task[]; total: number }> {
+		// Validate page and limit values
+		console.log("test");
 
-  @Get(':id')
-  async findOne(@Param('id') id: string): Promise<Task> {
-    return this.taskService.findOne(id);
-  }
+		page = page > 0 ? page : page;
+		limit = limit > 0 ? limit : limit;
 
-  @Post()
-  async create(@Body() task: Task): Promise<Task> {
-    return this.taskService.create(task);
-  }
+		const result = await this.taskService.findAllPaginated(+total, page, limit);
+		return {
+			data: result.data,
+			total: result.total,
+		};
+	}
 
-  @Put(':id')
-  async update(
-    @Param('id') id: string,
-    @Body() updatedTask: Partial<Task>,
-  ): Promise<Task> {
-    return this.taskService.update(id, updatedTask);
-  }
+	@Get(":id")
+	async findOne(@Param("id") id: string): Promise<Task> {
+		return this.taskService.findOne(id);
+	}
 
-  @Delete(':id')
-  async remove(@Param('id') id: string): Promise<void> {
-    return this.taskService.remove(id);
-  }
+	@Post()
+	async create(@Body() task: Task): Promise<Task> {
+		return this.taskService.create(task);
+	}
+
+	@Put(":id")
+	async update(@Param("id") id: string, @Body() updatedTask: Partial<Task>): Promise<Task> {
+		return this.taskService.update(id, updatedTask);
+	}
+
+	@Delete(":id")
+	async remove(@Param("id") id: string): Promise<void> {
+		return this.taskService.remove(id);
+	}
 }
